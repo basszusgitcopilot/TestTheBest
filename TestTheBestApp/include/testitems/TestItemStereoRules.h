edcb50569c4bcfebc74bd1b0ada656b4667ce4ef
Bar Mire <barmire123@t-online.de>
2026-04-04 17:24:22 +0200

    rules added

--- TestTheBestApp/include/testitems/TestItemStereoRules.h
+++ TestTheBestApp/include/testitems/TestItemStereoRules.h
@@ -21,10 +21,22 @@ class TestItemStereoRules : public TestItem {
     int32_t ruleIndex;
 
     const std::vector<std::pair<std::string, std::string>> rules{
-        {"das Volumen eines Würfels", "a³"},                 //
-        {"die Oberfläche eines Würfels", "6a²"},             //
-        {"das Volumen eines Quaders", "a⋅b⋅c"},              //
-        {"die Oberfläche eines Quaders", "2(ab + ac + bc)"}, //
+        {"das Volumen eines Würfels", "a³"},                                  //
+        {"die Oberfläche eines Würfels", "6a²"},                              //
+        {"das Volumen eines Quaders", "a⋅b⋅c"},                               //
+        {"die Oberfläche eines Quaders", "2(ab + ac + bc)"},                  //
+        {"das Volumen eines Prismas", "Grundfläche⋅h"},                       //
+        {"die Oberfläche eines Prismas", "2⋅Grundfläche + Mantelfläche"},     //
+        {"das Volumen einer Pyramide", "Grundfläche⋅h/3"},                    //
+        {"die Oberfläche eines Prismas", "Grundfläche + Mantelfläche"},       //
+        {"das Volumen eines Tetraeders", "(a³√2)/12"},                        //
+        {"die Oberfläche eines Tetraeders", "a²√3"},                          //
+        {"das Volumen eines Zylinders", "r²⋅π⋅h"},                            //
+        {"die Oberfläche eines Zylinders", "2⋅r⋅π⋅(h + r)"},                  //
+        {"das Volumen eines geraden Kreiskegels", "1/3⋅r²⋅π⋅h"},              //
+        {"die Oberfläche eines geraden Kreiskegels", "r⋅π⋅(r + √(h² + r²))"}, //
+        {"das Volumen eines Kugels", "4/3⋅r³⋅π"},                             //
+        {"die Oberfläche eines Kugels", "4⋅r²⋅π"},                            //
     };
 };
 } // namespace com::prog::testthebest
