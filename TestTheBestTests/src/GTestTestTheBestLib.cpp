b630f7bddee68273691732483f0d6d6059a227ff
Gergely <gergely@debian.mycomputer>
2026-04-16 10:28:35 +0200

    factorial test added

--- TestTheBestTests/src/GTestTestTheBestLib.cpp
+++ TestTheBestTests/src/GTestTestTheBestLib.cpp
@@ -1,3 +1,9 @@
+#include "MathUtils.h"
 #include <gtest/gtest.h>
 
-TEST(TestTheBestLib, first) { ASSERT_EQ(false, true); }
+TEST(TestTheBestLib, factorial) {
+    ASSERT_EQ(1, factorial(0));
+    ASSERT_EQ(1, factorial(1));
+    ASSERT_EQ(2, factorial(2));
+    ASSERT_EQ(6, factorial(3));
+}
